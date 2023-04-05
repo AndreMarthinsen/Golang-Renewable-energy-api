7b490c30609b1ca48c3ab6b24679de7e41190104
Paul Schiager <paulschi@stud.ntnu.no>
2023-04-05 17:49:16 +0200

    Implemented StatusClient function, along with data structure for service status. Added consts for 3rd party urls. Relates to #12

--- handlers/status.go
+++ handlers/status.go
@@ -1,8 +1,44 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type ServiceStatus struct {
+	CountriesApi int    `json:"countries_api"`
+	EnergyApi    int    `json:"notification_db"`
+	Webhooks     int    `json:"webhooks"`
+	Version      string `json:"version"`
+	Uptime       int    `json:"uptime"`
+}
 
 // HandlerStatus Handler for the status endpoint
 func HandlerStatus(w http.ResponseWriter, r *http.Request) {
 
+}
+
+// StatusClient Sends requests to 3rd party services
+func StatusClient(url string) (status int, err error) {
+	status = http.StatusServiceUnavailable
+
+	r, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println("Error while creating status request.")
+		return status, err
+	}
+
+	// Client instantiation:
+	client := http.Client{}
+	defer client.CloseIdleConnections()
+
+	// Request/response:
+	res, err := client.Do(r)
+	if err != nil {
+		fmt.Println("Unable to send request.")
+		return status, err
+	}
+
+	status = res.StatusCode
+	return status, nil
 }
