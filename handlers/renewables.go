4abb7b3f7e76266f34cdb1f2ae0520f8310fbfc4
Even Stetrud <evenstet@stud.ntnu.no>
2023-04-07 15:03:47 +0200

    Added util/util.go: fetched from https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2023-workspace/andrrma/deployment-test

--- handlers/renewables.go
+++ handlers/renewables.go
@@ -2,7 +2,8 @@ package handlers
 
 import "net/http"
 
-// HandlerRenew Handler for the renewables endpoint
+// HandlerRenew Handler for the renewables endpoint: this checks if the request is GET, and calls the correct funtion
+// for current renewable percentage or historical renewable percentage
 func HandlerRenew(w http.ResponseWriter, r *http.Request) {
-
+	
 }
