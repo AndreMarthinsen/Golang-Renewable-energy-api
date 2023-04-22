bfbf8a6d5ba18a00278ba41c45cd1f0cdaeb2663
andrrma <andrrma@stud.ntnu.no>
2023-04-22 15:29:29 +0200

    Partitions invocation_worker.go into smaller functions. Removes InvocationCounter struct as the Updated bool is redundant with the current map reset per update cycle. Relates to #11

--- caching/invocation_worker.go
+++ caching/invocation_worker.go
@@ -6,111 +6,55 @@ import (
 	"bytes"
 	"cloud.google.com/go/firestore"
 	"encoding/json"
+	"errors"
 	"google.golang.org/api/iterator"
 	"log"
 	"net/http"
 	"time"
 )
 
-type InvocationCounter struct {
-	Count   int32
-	Updated bool
+type webhookCheck struct {
+	ID   string
+	Body notifications.WebhookRegistration
 }
 
 // InvocationWorker receives updates from endpoint handlers and updates
 // an in memory data structure mapping country code to invocation count.
 // Registered webhooks are periodically checked in DB to see if they should
 // trigger, and if so, a message is sent to the registered url.
-func InvocationWorker(cfg *util.Config, stop chan struct{}, invocationChannel chan []string) {
+func InvocationWorker(cfg *util.Config, stop chan struct{}, countryDB map[string]util.Country, invocationChannel chan []string) {
 
-	invocationMap := make(map[string]InvocationCounter, 0)
+	// maps cca3 codes to the invocation count for a current cycle.
+	invocationMap := make(map[string]int32, 0)
+
 	client := http.Client{}
 
 	for {
 		select {
 		case <-time.After(time.Second * 1): // TODO: Config setting
-			var updatedCountries []string
-			for cca3, counter := range invocationMap {
-				if counter.Updated {
-					updatedCountries = append(updatedCountries, cca3)
-					counter.Updated = false
-					invocationMap[cca3] = counter
-				}
-			}
+			updatedCountries := getUpdatedCountries(invocationMap)
 
-			query := cfg.FirestoreClient.Collection(cfg.WebhookCollection).Where("country", "in", updatedCountries)
+			ref := cfg.FirestoreClient.Collection(cfg.WebhookCollection)
+			query := ref.Where("country", "in", updatedCountries)
 			iter := query.Documents(*cfg.Ctx)
-
-			type webhookCheck struct {
-				ID           string
-				Registration notifications.WebhookRegistration
-			}
 
 			bulkOperation := cfg.FirestoreClient.BulkWriter(*cfg.Ctx)
 
-			var webhooksToCheck []webhookCheck
-			for {
-				doc, err := iter.Next()
-				if err == iterator.Done {
-					break
-				}
-				if err != nil {
-					log.Println("notification worker: ", err)
-					break
-				}
-				webhook := notifications.WebhookRegistration{}
-				if err = doc.DataTo(&webhook); err != nil {
-					log.Println("notification worker: ", err)
-					break
-				}
-				_, err = bulkOperation.Update(
-					doc.Ref,
-					[]firestore.Update{
-						{
-							Path:  "call_count",
-							Value: webhook.CallCount + invocationMap[webhook.Country].Count,
-						},
-					})
-				if err != nil {
-					log.Println("notification worker: ", err)
-					break
-				}
-				webhooksToCheck = append(webhooksToCheck, webhookCheck{ID: doc.Ref.ID, Registration: webhook})
+			err, webhooksToCheck := getDocumentsToUpdate(iter, bulkOperation, invocationMap)
+			if err != nil {
+				log.Println("invocation worker:", err)
 			}
 			// Iterates through relevant webhooks and
 			for i, webhook := range webhooksToCheck {
-				oldCount := webhook.Registration.CallCount
-				newCount := invocationMap[webhook.Registration.Country].Count + oldCount
-				previousTriggers := oldCount / webhook.Registration.Calls
-				triggers := newCount/webhook.Registration.Calls - previousTriggers
-				if triggers != 0 {
-					for j := 0; int32(j) < triggers; j++ {
-						//TODO: Use Evens thing to get country name
-						message := notifications.WebhookTrigger{
-							WebhookId:  webhook.ID,
-							Country:    webhook.Registration.Country,
-							TotalCalls: previousTriggers + int32(j)*webhook.Registration.Calls,
-						}
-						payload, err := json.Marshal(message)
-						if err != nil {
-							log.Println("invocation worker: ", err)
-							break // TODO: Best response to this?
-						}
-						request, err := http.NewRequest(http.MethodPost, webhook.Registration.URL, bytes.NewBuffer(payload))
-						if err != nil {
-							log.Println("invocation worker: ", err)
-							break // TODO: Best response to this?
-						}
-						response, err := client.Do(request)
-						util.LogOnDebug(cfg, "invocation worker: response from recipient on trigger"+
-							" for url "+request.URL.Path+" : "+response.Status)
-					}
+				if err := doWebhookEvents(&client, webhook, countryDB, invocationMap); err != nil {
+					log.Println("invocation worker: ", err)
+				} else {
+					webhooksToCheck[i].Body.Count = invocationMap[webhook.Body.Country] + webhook.Body.Count
 				}
-				webhooksToCheck[i].Registration.CallCount = newCount
 			}
 
-			bulkOperation.End()                            // Executes write operations
-			invocationMap = map[string]InvocationCounter{} // reset of counters
+			bulkOperation.End()                // Executes write operations
+			invocationMap = map[string]int32{} // reset of counters
 
 		case invocations, ok := <-invocationChannel:
 			if ok != true {
@@ -118,12 +62,88 @@ func InvocationWorker(cfg *util.Config, stop chan struct{}, invocationChannel ch
 				return
 			} // updates invocation count and sets updated to true
 			for _, invocation := range invocations {
-				if oldCounter, ok := invocationMap[invocation]; ok {
-					invocationMap[invocation] = InvocationCounter{Count: oldCounter.Count + 1, Updated: true}
+				if _, ok = invocationMap[invocation]; ok {
+					invocationMap[invocation] += 1
 				} else {
-					invocationMap[invocation] = InvocationCounter{Count: 1, Updated: true}
+					invocationMap[invocation] = 1
 				}
 			}
 		}
 	}
+}
+
+// getUpdatedCountries retrieves a list of all countries that have been updated
+// TODO: Probably pointless due to the reset per cycle
+func getUpdatedCountries(invocations map[string]int32) []string {
+	updatedCountries := make([]string, 0)
+	for cca3 := range invocations {
+		updatedCountries = append(updatedCountries, cca3)
+	}
+	return updatedCountries
+}
+
+func getDocumentsToUpdate(iter *firestore.DocumentIterator, bulkOperation *firestore.BulkWriter,
+	invocationMap map[string]int32) (error, []webhookCheck) {
+	var webhooksToCheck []webhookCheck
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err, webhooksToCheck
+		}
+		webhook := notifications.WebhookRegistration{}
+		if err = doc.DataTo(&webhook); err != nil {
+			return err, webhooksToCheck
+		}
+		_, err = bulkOperation.Update(
+			doc.Ref,
+			[]firestore.Update{
+				{Path: "call_count", Value: webhook.Count + invocationMap[webhook.Country]},
+			})
+		if err != nil {
+			return err, webhooksToCheck
+		}
+		webhooksToCheck = append(webhooksToCheck, webhookCheck{ID: doc.Ref.ID, Body: webhook})
+	}
+	return nil, webhooksToCheck
+}
+
+func doWebhookEvents(client *http.Client, webhook webhookCheck,
+	countryDB map[string]util.Country, invocations map[string]int32) error {
+
+	oldCount := webhook.Body.Count
+	newCount := invocations[webhook.Body.Country] + oldCount
+	previousTriggers := oldCount / webhook.Body.Calls
+	triggers := newCount/webhook.Body.Calls - previousTriggers
+	if triggers != 0 {
+		for j := 0; int32(j) < triggers; j++ {
+			country, ok := countryDB[webhook.Body.Country]
+			var countryName string
+			if ok {
+				countryName = country.Name
+			} else {
+				return errors.New("invocation worker: cca3 -> full name not found in local DB")
+			}
+			message := notifications.WebhookTrigger{
+				WebhookId:  webhook.ID,
+				Country:    countryName,
+				TotalCalls: previousTriggers + int32(j)*webhook.Body.Calls,
+			}
+			payload, err := json.Marshal(message)
+			if err != nil {
+				return err
+			}
+			request, err := http.NewRequest(http.MethodPost, webhook.Body.URL, bytes.NewBuffer(payload))
+			if err != nil {
+				return err
+			}
+			response, err := client.Do(request)
+			if err != nil {
+				return errors.New(err.Error() + " : status: " + response.Status)
+			}
+		}
+	}
+	return nil
 }
