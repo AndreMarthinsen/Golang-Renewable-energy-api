8f360747dd4cbbafe3bf3a3ce1319e6562ff0dd4
Even Stetrud <evenstet@stud.ntnu.no>
2023-04-25 13:17:08 +0200

    Fixed error in restcountries domain-const, relates to #6

--- consts/consts.go
+++ consts/consts.go
@@ -3,8 +3,8 @@ package consts
 // External paths
 
 const CountryDomain = "http://129.241.150.113:8080/"
-const CountryNamePath = "/v3.1/name/"
-const CountryCodePath = "/v3.1/alpha/"
+const CountryNamePath = "v3.1/name/"
+const CountryCodePath = "v3.1/alpha/"
 const CountryFilteringByName = "?fields=name,languages,maps,borders&fullText=true"
 const CountryFilteringByCode = "?fields=name,languages,maps,borders&codes="
 
